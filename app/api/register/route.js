abd31db4ca96fef179d96b6b0b793ee78e27519f
santohack <santohack238@gmail.com>
2024-02-18 05:27:49 +0530

    register user handled

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -5,42 +5,44 @@ import { isEmail } from "validator";
 
 const saltRounds = 10;
 export const POST = async (req) => {
-  const { name, email, password } = await req.json();
-
-  if (name.trim().length === 0) {
-    return new Response(JSON.stringify({ message: "Name is required" }), {
-      status: 500,
-    });
-  }
-  if (email.trim().length === 0) {
-    return new Response(JSON.stringify({ message: "Email is required" }), {
-      status: 500,
-    });
-  }
-  if (!isEmail(email)) {
-    return new Response(JSON.stringify({ message: "Email is invalid" }), {
-      status: 500,
-    });
-  }
-  if (password.trim().length === 0) {
-    return new Response(JSON.stringify({ message: "Password is required" }), {
-      status: 500,
-    });
-  }
-  if (password.trim().length < 6) {
-    return new Response(
-      JSON.stringify({ message: "Password must be at least 6 characters" }),
-      { status: 500 }
-    );
-  }
-
   try {
+    const { name, email, password } = await req.json();
+
+    if (name.trim().length === 0) {
+      return new Response(JSON.stringify({ message: "Name is required" }), {
+        status: 400,
+      });
+    }
+    if (email.trim().length === 0) {
+      return new Response(JSON.stringify({ message: "Email is required" }), {
+        status: 400,
+      });
+    }
+    if (!isEmail(email)) {
+      return new Response(JSON.stringify({ message: "Email is invalid" }), {
+        status: 400,
+      });
+    }
+    if (password.trim().length === 0) {
+      return new Response(JSON.stringify({ message: "Password is required" }), {
+        status: 400,
+      });
+    }
+    if (password.trim().length < 6) {
+      return new Response(
+        JSON.stringify({ message: "Password must be at least 6 characters" }),
+        { status: 400 }
+      );
+    }
     await connectDB();
     const existingUser = await User.findOne({ email }).exec();
     if (existingUser) {
-      return new Response(JSON.stringify({ message: "User already exists" }), {
-        status: 500,
-      });
+      return new Response(
+        JSON.stringify({ message: "Email is already in use" }),
+        {
+          status: 400,
+        }
+      );
     }
 
     const hashedPassword = await bcrypt.hash(password, saltRounds);
@@ -51,7 +53,10 @@ export const POST = async (req) => {
     });
 
     await user.save();
-    return new Response(JSON.stringify(user,{message: "User created successfully"}), { status: 200 });
+    return new Response(
+      JSON.stringify(user, { message: "User created successfully" }),
+      { status: 200 }
+    );
   } catch (error) {
     return new Response(JSON.stringify(error), { status: 500 });
   }
